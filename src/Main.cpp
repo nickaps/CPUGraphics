b89f782d7356ca38950afab56d0e648da005e692
nick <nickisepic8@gmail.com>
2026-08-02 16:14:53 -0500

    Removed CRT diagnostics from code

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,14 +6,10 @@
 
 int main(int argc, char* argv[]) {
 
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
 	{
 		Engine engine((char*)"New Window\0", 800, 600);
 		engine.eRunGame();
 	}
-
-	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
